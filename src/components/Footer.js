6e932938db89c77fcaef6cb7efb80722eff6e5a6
Franklin Agyare <fagyare@users.noreply.github.com>
2023-02-27 23:56:59 -0500

    footer and README.md updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 function Footer (){
     return (
-        <div>Contacts || Forum || Policies || Terms </div>
+        <div className="footer"> 
+        <div>Contacts || Forum || Privacy Policy || Terms of Use </div>
+
+        <p>© Copyright FA Production and Technology EST. 2023</p>
+        </div>
     );
+
 }
 
 export default Footer; 
